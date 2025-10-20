0ffd0431f7c86f3dd8ee07af2b7026e424ef1183
Carlos Campoverde Polo <89158049+Sinmobrel@users.noreply.github.com>
2025-10-20 03:49:22 -0300

    Delete mean-stack directory

--- mean-stack/frontend/src/main.ts
+++
@@ -1,7 +0,0 @@
-// Zone.js ya se importa en polyfills.ts
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
-
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
